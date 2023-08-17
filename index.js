691f632a74d68fe2b10994969ed079f7df673e55
khalilb2000 <128535809+khalilb2000@users.noreply.github.com>
2023-08-17 10:35:22 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,59 @@
-const passwordBox = document.getElementById("password")
-  const length = 12;
+const passwordBox = document.getElementById("password");
 
-  const symbol = "@#$";
-  const number = "0123456789";
-  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  const allChars = upperCase + number + symbol;
-  
-  function createPassword(){
-    let password = "";
-    password += symbol[Math.floor(Math.random() * symbol.length)]
-    password += number[Math.floor(Math.random() * number.length)]
-    password += upperCase[Math.floor(Math.random() * upperCase.length)]
+const userLengthString = prompt ("Enter password length (between 8 and 128):")
+if(!userLengthString){
+  alert("Password length must be entered")
+}
 
-    while (length > password.length){
-        password += allChars[Math.floor(Math.random() * allChars.length)];
-    }
-    passwordBox.value = password;
-console.log(password);
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const symbol = "@#$";
+const number = "0123456789";
+
+function generatePassword() {
+  const userLength = parseInt(prompt("Enter password length (between 8 and 128):"));
+
+  if (userLength < 8 || userLength > 128 || isNaN(userLength)) {
+    alert("Please enter a valid length between 8 and 128.");
+    return;
   }
+
+  const specialChars= prompt("Enter the special charaters you want to include!");
+
+  if (!specialChars){
+    alert("At least one special charater is required");
+    return;
+  }
+
+  
+
+
+  const includeLowercase = prompt("Include lowercase letters? (yes/no)").toLowerCase() === 'yes';
+const includeUppercase = prompt("Include uppercase letters? (yes/no)").toLowerCase() === 'yes';
+const includeNumbers = prompt("Include numbers? (yes/no)").toLowerCase() === 'yes';
+
+
+  if (!includeLowercase && !includeUppercase && !includeSymbols && !includeNumbers) {
+    alert("At least one character type should be selected.");
+    return;
+  }
+
+  let charBank = "";
+  if (includeLowercase) charBank += lowercase;
+  if (includeUppercase) charBank += uppercase;
+  if (includeSymbols) charBank += symbol;
+  if (includeNumbers) charBank += number;
+
+  let password = "";
+  for (let i = 0; i < userLength; i++) {
+    const randomIndex = Math.floor(Math.random() * charBank.length);
+    password += charBank[randomIndex];
+  }
+
+  passwordBox.value = password;
+  console.log(password);
+}
+
+// Add an event listener to the button
+document.getElementById("generate-btn").addEventListener("click", generatePassword);
